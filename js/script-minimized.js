283c0e8af7b30c2d90cd99d3c88d090d2e786ce8
Nastya-B <anastasia.borodulina@gmail.com>
2016-02-22 20:13:46 +0300

    Sedona!

---
+++ js/script-minimized.js
@@ -0,0 +1,1 @@
+var button=document.querySelector(".search-button"),popup=document.querySelector(".search-form");button.addEventListener("click",function(a){a.preventDefault(),popup.classList.toggle("show")});
